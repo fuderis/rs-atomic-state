1dfb26f887605a9fdcff1dec18bc3aa45ba5a077
Bulat Sh. <synapdrake@ya.ru>
2025-09-03 07:24:58 +0500

    Removed extra dirs

--- src/state copy/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod state;   pub use state::AtomState;
-pub mod guard;   pub use guard::AtomStateGuard;
-pub mod reff;    pub use reff::AtomStateRef;
-
-pub(super) const ERR_MSG: &str = "The data has been poisoned!";
